f3de7883a3c3d6eda569a29b29fc1d0c599d2503
Ross <phambatoanbk@gmail.com>
2019-10-11 01:01:36 +0700

    Register success and failed

--- public/xuly.js
+++ public/xuly.js
@@ -1,6 +1,20 @@
 var socket = io("http://localhost:3000");
 
+socket.on("client-send-dki-thatbai", () => {
+    alert(" Sai username (co nguoi da dang ki roi!!!)");
+});
+
+socket.on("server-send-dki-thanhcong", (data) => {
+    $("#currentUser").html(data);
+    $("#loginForm").hide(2); //2s
+    $("#chatForm").show(1); //s
+});
+
 $(document).ready(() => {
     $("#loginForm").show();
     $("#chatForm").hide();
+
+    $("#btnRegister").click(() => {
+        socket.emit("client-send-Username", $("#txtUsername").val());
+    });
 });
--- server.js
+++ server.js
@@ -9,8 +9,21 @@ var server = require("http").Server(app);
 var io = require("socket.io")(server);
 server.listen(3000);
 
+var mangUsers = ["AAA"];
+
 io.on("connection", (socket) => {
     console.log("Co nguoi ket noi" + socket.id);
+
+    socket.on("client-send-Username", (data) => {
+        if (mangUsers.indexOf(data) >= 0) {
+            // Dang ki that bai
+            socket.emit("client-send-dki-thatbai");
+        } else {
+            // Dang ki thanh cong
+            mangUsers.push(data);
+            socket.emit("server-send-dki-thanhcong", data);
+        }
+    });
 });
 
 app.get("/", (req, res) => {
